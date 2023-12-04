d5fb2fa9780622116a8978f6eb3c148bf4e57964
Tomas Vosicky <vosictom@fit.cvut.cz>
2023-12-04 22:23:49 +0100

    language per project

--- src/views/ProjectSettings.vue
+++ src/views/ProjectSettings.vue
@@ -3,6 +3,15 @@
     <ScrollArea>
       <div>
         <h2 class="subheader px-2">Project settings</h2>
+
+        <v-select
+          class="filled"
+          label="Language"
+          v-model="settings.lang"
+          :items="availableLanguages"
+          itemValue="code"
+          itemText="title"
+        />
       </div>
     </ScrollArea>
   </div>
@@ -16,6 +25,27 @@ export default {
   props: {
     project: Object,
     settings: Object,
+  },
+  setup() {
+    return {
+      availableLanguages: [
+        {
+          id: 'en',
+          code: 'en-us',
+          title: 'English',
+        },
+        {
+          id: 'cs',
+          code: 'cs-cz',
+          title: 'Czech',
+        },
+        {
+          id: 'sk',
+          code: 'sk-sk',
+          title: 'Slovak',
+        },
+      ],
+    }
   },
 }
 </script>
