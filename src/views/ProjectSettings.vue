e3acbdb225d744b1b34a2cc0bcedae774792189c
Tomas Vosicky <vosictom@fit.cvut.cz>
2023-12-09 17:59:50 +0100

    project settings - description

--- src/views/ProjectSettings.vue
+++ src/views/ProjectSettings.vue
@@ -3,6 +3,13 @@
     <ScrollArea>
       <div>
         <h2 class="subheader px-2">Project settings</h2>
+        <TextEditor
+          class="filled"
+          label="Project description"
+          v-model="settings.description"
+          multiline
+          lazy
+        />
       </div>
     </ScrollArea>
   </div>
@@ -10,12 +17,33 @@
 
 <script>
 import ScrollArea from '@/ui/ScrollArea.vue'
+import TextEditor from '@/components/TextEditor.vue'
 
 export default {
-  components: { ScrollArea },
+  components: { ScrollArea, TextEditor },
   props: {
     project: Object,
     settings: Object,
   },
 }
 </script>
+
+<style lang="scss" scoped>
+:deep {
+  .text-editor {
+    > label {
+      order: 0;
+    }
+
+    > .input {
+      order: 2;
+    }
+
+    > .toolbar {
+      order: 1;
+      top: 0;
+      position: sticky;
+    }
+  }
+}
+</style>
